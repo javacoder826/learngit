fb21fd8dcf192e9a578bca85515429ab34ce0910
javacoder <1004349374@qq.com>
2015-08-24 23:19:17 +0800

    add Person.java

---
+++ Person.java
@@ -0,0 +1,12 @@
+public class Person {
+	private String name;
+	private Integer age;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
